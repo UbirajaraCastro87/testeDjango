7a98e7e06a297045085e08c7277519476dec4c40
rodrigocomaf <167134167+UbirajaraCastro87@users.noreply.github.com>
2026-03-26 16:52:28 -0300

    Primeiro commit do projeto Django

---
+++ mysite/polls/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Choice, Question
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
